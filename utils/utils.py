ef29793ad994add04dcce00f78b24ca90711fb61
Praveenkusuluri08 <praveenkusuluri07@gmail.com>
2024-09-04 11:03:43 -0400

    Intial graph representation and frontend setup

--- utils/utils.py
+++ utils/utils.py
@@ -2,8 +2,32 @@ import requests
 import networkx as nx
 import matplotlib.pyplot as plt
 
+def create_graph():
+    G = nx.Graph()
+
+    data = get_data()
+
+    for package in data:
+        main_package = package["_id"]
+
+        G.add_node(main_package, label=main_package)
+        for dependency, _ in package["dependencies"].items():
+            G.add_node(dependency, label=dependency)
+            G.add_edge(dependency, main_package)
+
+    pos = nx.spring_layout(G)  
+    nx.draw(G, pos, with_labels=False, node_size=500, node_color="lightblue") 
+
+    labels = nx.get_node_attributes(G, 'label')
+    nx.draw_networkx_labels(G, pos, labels, font_size=10)
+
+    nx.draw_networkx_nodes(G, pos, nodelist=[main_package], node_color='red', node_size=700)
+
+    plt.savefig("graph.png")
+    
+    
 def get_data():
-    package_names = ["express","dotenv","loadash","axios"]
+    package_names = ["express"]
 
     data = []
 
@@ -28,3 +52,6 @@ def get_data():
             print(f"Failed to fetch data for {package_name}, status code: {response.status_code}")
 
     return data
+
+
+create_graph()
