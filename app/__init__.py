d97ec61889a8f6d9dc5ef397a80c357a46ff2bfa
Nick <ngstafford1@gmail.com>
2024-09-08 20:33:39 -0400

    added flask blueprints. We are ready to start writing the scraper

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
 
-from app import routes
+def create_app():
+    app=Flask(__name__)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    # Configure our app here. if we want to use blueprints later, we can do that. 
+    return app
+
