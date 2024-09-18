8f20f4e7e91a04c75ed09d34871252cd6c78b616
= <ngstafford1@gmail.com>
2024-09-17 21:37:12 -0400

    ruff format change test

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,13 +1,13 @@
-import requests
-import networkx as nx
 import matplotlib.pyplot as plt
+import networkx as nx
+import requests
 
 from .package import Package
 
 
 def create_graph():
     G = nx.MultiDiGraph()
-    
+
     data = get_data()
 
     for package in data:
@@ -18,16 +18,19 @@ def create_graph():
             G.add_node(dependency, label=dependency)
             G.add_edge(dependency, main_package)
 
-    pos = nx.spring_layout(G)  
-    nx.draw(G, pos, with_labels=False, node_size=500, node_color="lightblue") 
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=False, node_size=500, node_color="lightblue")
 
-    labels = nx.get_node_attributes(G, 'label')
+    labels = nx.get_node_attributes(G, "label")
     nx.draw_networkx_labels(G, pos, labels, font_size=10)
 
-    nx.draw_networkx_nodes(G, pos, nodelist=[main_package], node_color='red', node_size=700)
+    nx.draw_networkx_nodes(
+        G, pos, nodelist=[main_package], node_color="red", node_size=700
+    )
 
     plt.savefig("graph.png")
-    
+
+
 def get_data():
     package_names = ["express"]
     data = []
@@ -36,32 +39,37 @@ def get_data():
         response = requests.get(url)
         if response.status_code == 200:
             r_dict = response.json()
-            
+
             latest_version = r_dict.get("dist-tags", {}).get("latest")
-            
-            dependencies = r_dict.get("versions", {}).get(latest_version, {}).get("dependencies", None)
-            
+
+            dependencies = (
+                r_dict.get("versions", {})
+                .get(latest_version, {})
+                .get("dependencies", None)
+            )
+
             p = Package(
-                r_dict.get("_id"),
-                r_dict.get("description"),
-                latest_version,
-                dependencies
+                r_dict.get("_id"), r_dict.get("description"), latest_version, dependencies
             )
             data.append(p)
         else:
-            print(f"Failed to fetch data for {package_name}, status code: {response.status_code}")
+            print(
+                f"Failed to fetch data for {package_name}, status code: {response.status_code}"
+            )
 
     return data
 
-def fetch_dependencies(package_name, version='latest'):
-    url = f'https://registry.npmjs.org/{package_name}/{version}'
+
+def fetch_dependencies(package_name, version="latest"):
+    url = f"https://registry.npmjs.org/{package_name}/{version}"
     response = requests.get(url)
     package_data = response.json()
 
-    dependencies = package_data.get('dependencies', {})
+    dependencies = package_data.get("dependencies", {})
     return dependencies
 
-def build_graph(package_name, version='latest'):
+
+def build_graph(package_name, version="latest"):
     G = nx.DiGraph()
     G.add_node(package_name)
 
