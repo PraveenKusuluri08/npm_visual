5fffb6cfdd43c44d07aa7c5484ad947bc4ed3723
= <ngstafford1@gmail.com>
2024-09-17 21:27:27 -0400

    added ruff as a formater and linter

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,23 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 import os
 from app.utils import utils
 import networkx as nx
 
+
 def create_app():
-    app=Flask(__name__)
+    app = Flask(__name__)
 
     from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
 
+    app.register_blueprint(main_bp)
 
     @app.route("/npm")
     def npm():
         return jsonify(utils.utils.get_data())
 
-    @app.route('/dependencies/<package_name>', methods=['GET'])
+    @app.route(
+        "/dependencies/<package_name>", methods=["GET"]
+    )
     # print('api route called')
     def get_package_dependencies(package_name):
         graph = utils.build_graph(package_name)
@@ -22,6 +25,5 @@ def create_app():
         graph_data = nx.node_link_data(graph)
         return jsonify(graph_data)
 
-    # Configure our app here. if we want to use blueprints later, we can do that. 
+    # Configure our app here. if we want to use blueprints later, we can do that.
     return app
-
