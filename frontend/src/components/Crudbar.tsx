8c4061a190e27a69bc056aad22efcf7f6881cc3d
= <ngstafford1@gmail.com>
2024-09-22 23:55:19 -0400

    added a function to call dynamically

--- frontend/src/components/Crudbar.tsx
+++ frontend/src/components/Crudbar.tsx
@@ -1,13 +1,37 @@
 import axios from "axios";
 
-function scrapeAll() {
-  const url = "/api/scrapeAll";
-  axios.get(url)
-}
 
 function Crudbar() {
+
+  function scrapeAll() {
+    const url = "/api/scrapeAll";
+    axios.get(url)
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const input = form.elements.namedItem('packageName') as HTMLInputElement;
+    const packageName = input.value;
+    alert("you want to search " + packageName)
+    const url = `/api/dependencies/${packageName}`;
+
+    axios.get(url)
+  }
+
   return <div><h2>Temp component to call backend routes</h2>
     <button onClick={() => scrapeAll()}>Load and Save Everything</button>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Package to Graph:
+        <input
+          name="packageName"
+          type="text"
+          defaultValue="Express"
+        />
+      </label>
+      <button type="submit" >Search</button>
+    </form>
   </div>
 }
 
