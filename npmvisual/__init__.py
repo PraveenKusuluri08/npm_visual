88e5f018ee0a7b45a0f00baf6f0b54b3cbd80059
= <ngstafford1@gmail.com>
2024-09-26 15:01:15 -0400

    created basic logging

--- npmvisual/__init__.py
+++ npmvisual/__init__.py
@@ -1,3 +1,7 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask, jsonify
 
 from .utils import (
@@ -8,6 +12,8 @@ from .utils import (
 
 def create_app():
     app = Flask(__name__)
+    load_logs(app)
+    app.logger.info("app created")
 
     @app.route("/scrapeAll", methods=["GET"])
     def scrape_all():
@@ -21,3 +27,20 @@ def create_app():
 
     # Configure our app here. if we want to use blueprints later, we can do that.
     return app
+
+
+def load_logs(app):
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler("logs/app.log", maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s " "[in %(pathname)s:%(lineno)d]"
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("app startup")
+    return
