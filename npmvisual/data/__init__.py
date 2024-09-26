c26ab51e8f074dcef79b4c887f4e4fef891aa70d
= <ngstafford1@gmail.com>
2024-09-26 14:00:54 -0400

    worlds smallest commit

--- npmvisual/data/__init__.py
+++ npmvisual/data/__init__.py
@@ -2,7 +2,7 @@
 This is the tool you should use to load packages. It will retrieve it from the db or from
 the web if it is not already cached.
 
-we have several data sources. db, cached package.json pages, and online scraping. This
+We have several data sources. db, cached package.json pages, and online scraping. This
 tool decides where to retrieve data from.
 """
 
