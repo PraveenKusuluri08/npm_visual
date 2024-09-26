7efded2aa1a0d04f2b4c12befcf3e13ef78e7774
= <ngstafford1@gmail.com>
2024-09-26 13:23:29 -0400

    setup logging but did not activate

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+# import logging
+# from logging import config as logging_config
+
 from npmvisual import create_app
 
+# import flask
+# app = flask.Flask(__name__)
 app = create_app()
+
+
+# if __name__ == "__main__":
+#     log_server = logging_config.listen()
+#     log_server.start()
+#     # Set the level to INFO by default.
+#     app.logger.setLevel(logging.INFO)
+#     try:
+#         app.run()
+#     finally:
+#         logging_config.stopListening()
+#         log_server.join()
