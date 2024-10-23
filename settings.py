ad6844c24bcf8eb5e617bb30aff9fdbcae688684
Nick <ngstafford1@gmail.com>
2024-10-23 02:04:21 -0400

    added config for neo4j

---
+++ settings.py
@@ -0,0 +1,7 @@
+from os import environ
+
+NEO4J_USERNAME = environ.get("NEO4J_USERNAME")
+NEO4J_PASSWORD = environ.get("NEO4J_PASSWORD")
+NEO4J_HOST = environ.get("NEO4J_HOST")
+NEO4J_DB = environ.get("NEO4J_DB")
+NEO4J_PORT = environ.get("NEO4J_PORT")
