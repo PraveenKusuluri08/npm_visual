eeabe582ac768a693aaaaa9ef5b1437471ccec04
Nick <ngstafford1@gmail.com>
2024-10-25 22:26:52 -0400

    added migration creation

--- npmvisual/migrations/migrations.py
+++ npmvisual/migrations/migrations.py
@@ -1,5 +1,7 @@
 import time
 
+from neo4j._sync.work.transaction import ManagedTransaction
+
 from npmvisual import db
 from npmvisual.migrations import bp
 from npmvisual.utils2 import ns_hash
@@ -7,29 +9,41 @@ from npmvisual.utils2 import ns_hash
 
 @bp.route("/test", methods=["GET"])
 def create_first_db_timestamp():
-    message = "created first db migration"
+    description = "created first db migration"
     # query = create_timestamp_query(message)
 
     # r = db.run(query)
-    name = message
-    r = db.execute_write(create_timestamp_query, name)
+    r = db.execute_write(create_migration_timestamp, description)
     print(r)
     return "success"
 
 
-def create_timestamp_query(tx, name):
+def create_migration_timestamp(
+    tx: ManagedTransaction,
+    description,
+):
     timestamp = time.time()
+    hash = ns_hash(description + str(timestamp), 24)
+    id = f"${timestamp}_${hash}_${description[24]}"
+
     result = tx.run(
         """
-        MERGE (p:Person {name: $name})
-        RETURN p.name AS nameP
+        MERGE (m:Migration{
+            migration_id: $id,
+            description: $description,
+            timestamp: $timestamp
+          })
+        ON CREATE SET m.created = timestamp()
+        ON MATCH SET
+        m.counter = coalesce(m.counter, 0) + 1,
+        m.accessTime = timestamp()
+        RETURN m
         """,
-        name=name,
+        id=id,
+        description=description,
+        timestamp=timestamp,
     )
     return result
-    # timestamp = time.time()
-    # hash = ns_hash(message + str(timestamp))
-    # name = f"${timestamp}_${hash}_${message}"
     # q = " \
     #     "CREATE (%s:MigrationTimestamp " \
     #     "{timestamp: '${timestamp}', message:'${message}'})"
