de34c878e8b571e31065e88e6b591d9f6edd4da4
Nick <ngstafford1@gmail.com>
2024-11-02 17:35:08 -0400

    added comment

--- npmvisual/models/__init__.py
+++ npmvisual/models/__init__.py
@@ -1,2 +1,9 @@
 from npmvisual._models.dependency import Dependency
 from npmvisual._models.package import Package
+
+"""
+This exists to resolve circular imports. Import from this file instead of npmvisual/_models/
+
+This is an implementation of the interface pattern. 
+
+"""
