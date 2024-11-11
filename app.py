5b02051af5664fe67c534288968d239b780f0c11
Nick <ngstafford1@gmail.com>
2024-11-11 01:28:02 -0500

    made the memory profiler only run in debug mode.

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-import os
-import time
-
-import psutil
-
 from npmvisual import create_app
 
 app = create_app()
 
-pid = os.getpid()
-process = psutil.Process(pid)
+if app.config["DEBUG"]:
+    import os
+    import time
 
-while True:
-    print(f"Memory usage: {process.memory_info().rss / (1024 ** 2):.2f} MB")
-    time.sleep(30)
+    import psutil
+
+    # Start memory usage monitoring
+    pid = os.getpid()
+    process = psutil.Process(pid)
+
+    while True:
+        print(f"Memory usage: {process.memory_info().rss / (1024 ** 2):.2f} MB")
+        time.sleep(30)
