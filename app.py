40ce6f38fe9b30695d2cc3c92b4bdc5637d2a7df
Nick <ngstafford1@gmail.com>
2024-11-14 19:35:04 -0500

    removed useless code

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
-# from npmvisual import create_an_item, create_app
 from npmvisual import create_app
 
-# app = create_app()
-
-# create_an_item()
-# print("create_an_item() completed")
-print("create_app called")
 app = create_app()
-print("create_app finished")
-
-if False and app.config["DEBUG"]:
-    import os
-    import time
-
-    import psutil
-
-    # Start memory usage monitoring
-    pid = os.getpid()
-    process = psutil.Process(pid)
-
-    while True:
-        print(f"Memory usage: {process.memory_info().rss / (1024 ** 2):.2f} MB")
-        time.sleep(30)
