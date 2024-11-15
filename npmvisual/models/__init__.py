cba88fabfd382f16b672c7aa7db9f32d5076c455
Nick <ngstafford1@gmail.com>
2024-11-14 19:59:27 -0500

    cleaned up code

--- npmvisual/models/__init__.py
+++ npmvisual/models/__init__.py
@@ -1,20 +1,9 @@
-# import neomodel
-# from datetime import datetime
-#
-
-# class NeomodelConnectionTest(neomodel.StructuredNode):
-#     __primarykey__ = "name"
-#     name = neomodel.StringProperty(unique_index=True)
-#     name2 = neomodel.StringProperty(unique_index=True)
-#
-#     # Add a created_at timestamp field
-#     created_at = neomodel.DateTimeProperty(default=datetime.now)
-#
-
 from npmvisual._models.dependency import Dependency
-from npmvisual._models.package import Package
 from npmvisual._models.neomodel_connection_test import NeomodelConnectionTest
-
+from npmvisual._models.ns_base_model import NSPrettyPrintable
+from npmvisual._models.package import Package
+from npmvisual._models.packageNode import PackageNode
+from npmvisual._models.packument import Packument
 
 """
 This exists to resolve circular imports. Import from this file instead of npmvisual/_models/
