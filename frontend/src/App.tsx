23e806a4bfd61b6a370b862df09f845c1457f21d
Nicholas Stafford <ngstafford1@gmail.com>
2024-12-18 18:52:42 -0500

    commented out frontend for now.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,75 +8,73 @@ import axios from "axios";
 import NpmVisualGraph3d from "./components/NpmVisualGraph3d";
 
 function App() {
-  const [packageName, setPackageName] = useState("");
+	const [packageName, setPackageName] = useState("");
 
-  const onPackageChanged = (packageName: string) => {
-    setPackageName(packageName);
-  };
-  const [graphData, setGraphData] = useState<GraphData>();
+	const onPackageChanged = (packageName: string) => {
+		setPackageName(packageName);
+	};
+	const [graphData, setGraphData] = useState<GraphData>();
 
-  useEffect(() => {
-    if (packageName !== "") {
-      const url = `/analyzeNetwork/react`;
-      axios
-        .get(url)
-        .then((data) => console.log("analysis result", data))
-        .catch((err) => {
-          console.log("error fetching data", err);
-        });
-    }
-  }, [packageName]);
+	useEffect(() => {
+		if (packageName !== "") {
+			const url = `/analyzeNetwork/react`;
+			axios
+				.get(url)
+				.then((data) => console.log("analysis result", data))
+				.catch((err) => {
+					console.log("error fetching data", err);
+				});
+		}
+	}, [packageName]);
 
-  useEffect(() => {
-    if (packageName != "") {
-      console.log("setting axios call");
-      let url;
-      if (packageName == "getPopularNetwork") url = "/api/getPopularNetworks";
-      else if (packageName == "getAllDBNetworks") url = "/api/getAllDBNetworks";
-      else url = `/api/getNetwork/${packageName}`;
-      // Prevent many calls to the same API.
-      const apiCache = getCache();
-      if (!apiCache.doesCallExist(url)) {
-        apiCache.addCall(url);
-        axios
-          .get(url)
-          .then((data) => {
-            console.log("setting package data");
-            setGraphData(data.data);
-            console.log("graph data set" + data.data);
-          })
-          .catch((error) => {
-            console.log("Error fetching data", error);
-          })
-          .finally(() => {
-            apiCache.removeCall(url);
-          });
-      } else {
-        console.log(
-          `did not sent request to ${url}, request already in progress`
-        );
-      }
-    }
-  }, [packageName]);
+	useEffect(() => {
+		if (packageName != "") {
+			console.log("setting axios call");
+			let url;
+			if (packageName == "getPopularNetwork") url = "/api/getPopularNetworks";
+			else if (packageName == "getAllDBNetworks") url = "/api/getAllDBNetworks";
+			else url = `/api/getNetwork/${packageName}`;
+			// Prevent many calls to the same API.
+			const apiCache = getCache();
+			if (!apiCache.doesCallExist(url)) {
+				apiCache.addCall(url);
+				axios
+					.get(url)
+					.then((data) => {
+						console.log("setting package data");
+						setGraphData(data.data);
+						console.log("graph data set" + data.data);
+					})
+					.catch((error) => {
+						console.log("Error fetching data", error);
+					})
+					.finally(() => {
+						apiCache.removeCall(url);
+					});
+			} else {
+				console.log(
+					`did not sent request to ${url}, request already in progress`,
+				);
+			}
+		}
+	}, [packageName]);
 
+	console.log("graphData", graphData);
 
-
-  console.log("graphData", graphData);
-
-  return (
-    <>
-      <div className="page">
-        <Crudbar onSelect={onPackageChanged} />
-        <h1>{packageName}</h1>
-        <div className="force-graph-3d-container">
-          <NpmVisualGraph3d graphData={graphData}></NpmVisualGraph3d>
-        </div>
-        {/* <div className="force-graph-2d-container"> */}
-        {/*   <GraphDiagram graphData={graphData} /> */}
-        {/* </div> */}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="page">
+				<Crudbar onSelect={onPackageChanged} />
+				<h1>{packageName}</h1>
+				<div className="force-graph-3d-container">
+					{/* <NpmVisualGraph3d graphData={graphData}></NpmVisualGraph3d> */}
+				</div>
+				{/* <div className="force-graph-2d-container"> */}
+				{/*   <GraphDiagram graphData={graphData} /> */}
+				{/* </div> */}
+			</div>
+		</>
+	);
 }
 
 export default App;
